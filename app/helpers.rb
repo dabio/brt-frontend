57dbabe7720afdc9cfaa751adcb03c51ce83ff0c
dabio <dbraband@gmail.com>
2013-04-02 08:47:16 +0200

    Fix the helper.

--- app/helpers.rb
+++ app/helpers.rb
@@ -2,7 +2,7 @@
 
 module ModuleName
 
-  class App
+  class Boot
 
     helpers Sinatra::RedirectWithFlash
 
@@ -13,17 +13,17 @@ module ModuleName
       # This gives us the currently logged in user. We keep track of that by just
       # setting a session variable with their is. If it doesn't exist, we want to
       # return nil.
-#      def current_person
-#        unless @cp and @request.session[:person_id]
-#          @cp = User.get(@request.session[:person_id])
-#        end
-#        @cp
-#      end
+      def current_user
+        unless @cp and @request.session[:user_id]
+          @cp = User.get(@request.session[:user_id])
+        end
+        @cp
+      end
 
-      # Checks if this is a logged in person
-#      def has_auth?
-#        !current_person.nil?
-#      end
+      # Checks if this is a logged in user
+      def has_auth?
+        !current_user.nil?
+      end
     end
   end
 end
