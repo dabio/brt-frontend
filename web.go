f008e3fe001634798cc972442a3ad849c475f4ee
Danilo <dabio@users.noreply.github.com>
2017-02-05 14:51:02 +0100

    Fix

--- web.go
+++ web.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -12,6 +13,127 @@ import (
 
 	_ "github.com/lib/pq"
 )
+
+// VEvent provides the grouping of component properties that describe the
+// event.
+type VEvent interface {
+	DTStamp() string
+	DTStart() string
+	DTEnd() string
+	Summary() string
+	URL() string
+	Attendees() []*Attendee
+}
+
+// Attendee defines an "attendee" within a calendar component.
+type Attendee interface {
+	CN() string
+}
+
+type event struct {
+	id        int
+	title     string
+	date      *time.Time
+	createdAt *time.Time
+	url       string
+	distance  int
+	people    []*person
+}
+
+// DTStamp specifies the date and time the instance of the iCalendar object
+// was created.
+func (e *event) DTStamp() string {
+	return e.createdAt.Format("20060102T150405Z")
+}
+
+// DTStart returns the start event date formatted for the use in .ics calendar
+// format.
+func (e *event) DTStart() string {
+	return e.date.Format("20060102")
+}
+
+// DTEnd returns the next day of the event. For use in .ics calendar format
+// to mark the ending date.
+func (e *event) DTEnd() string {
+	return e.date.AddDate(0, 0, 1).Format("20060102")
+}
+
+// Summary defines a short summary for the calendar component.
+func (e *event) Summary() string {
+	return fmt.Sprintf("%s\\, %dkm", e.title, e.distance)
+}
+
+// URL defines an URL associated with the iCalendar object.
+func (e *event) URL() string {
+	return e.url
+}
+
+// Attendees defines a list of "attendees" within a calendar component.
+func (e *event) Attendees() []*person {
+	return e.people
+}
+
+type person struct {
+	name  string
+	email string
+}
+
+func (p *person) CN() string {
+	return fmt.Sprintf("%s:mailto:%s", p.name, p.email)
+}
+
+func getCalendarEvents(db *sql.DB, year int) (events []event, err error) {
+	query := `
+	SELECT
+		e.id, e.title, e.date, e.created_at, e.url, e.distance,
+		p.first_name || ' ' || p.last_name AS name, p.email
+	FROM
+		events AS e
+		LEFT JOIN participations AS t ON e.id = t.event_id
+		LEFT JOIN people AS p ON t.person_id = p.id
+	WHERE
+		e.date BETWEEN $1 AND $2
+	ORDER BY
+		e.date, e.id, p.first_name, p.last_name
+	`
+	begin := time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
+	rows, err := db.Query(query, begin, begin.AddDate(1, 0, 0))
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+
+	var lastEvent event
+	var name, email sql.NullString
+	for rows.Next() {
+		var e event
+		if err = rows.Scan(&e.id, &e.title, &e.date, &e.createdAt, &e.url, &e.distance, &name, &email); err != nil {
+			return
+		}
+
+		// Init lastEvent when none was set before.
+		if lastEvent.id == 0 {
+			lastEvent = e
+		}
+
+		if e.id != lastEvent.id {
+			events = append(events, lastEvent)
+			lastEvent = e
+		}
+
+		if name.Valid && email.Valid {
+			lastEvent.people = append(lastEvent.people, &person{name: name.String, email: email.String})
+		}
+	}
+	if err = rows.Err(); err != nil {
+		return
+	}
+	if lastEvent.id != 0 {
+		events = append(events, lastEvent)
+	}
+
+	return
+}
 
 type context struct {
 	db        *sql.DB
@@ -34,7 +156,7 @@ func (c *context) calendar(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/calendar")
 
 	year, _ := strconv.Atoi(time.Now().Format("2006"))
-	events, err := GetCalendarEvents(c.db, year)
+	events, err := getCalendarEvents(c.db, year)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
