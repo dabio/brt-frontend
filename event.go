9aa9f1267a9daaa83b9d3249898f92a44f3e8a2e
Danilo <dabio@users.noreply.github.com>
2017-02-06 14:22:59 +0100

    Fixes

--- event.go
+++ event.go
@@ -3,32 +3,8 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"html/template"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
 	"time"
-
-	_ "github.com/lib/pq"
 )
-
-// VEvent provides the grouping of component properties that describe the
-// event.
-type VEvent interface {
-	DTStamp() string
-	DTStart() string
-	DTEnd() string
-	Summary() string
-	URL() string
-	Organizer() Attendee
-	Attendees() []*Attendee
-}
-
-// Attendee defines an "attendee" within a calendar component.
-type Attendee interface {
-	CN() string
-}
 
 type event struct {
 	id        int
@@ -83,15 +59,6 @@ func (e *event) Organizer() Attendee {
 // Attendees defines a list of "attendees" within a calendar component.
 func (e *event) Attendees() []*person {
 	return e.people
-}
-
-type person struct {
-	name  string
-	email string
-}
-
-func (p *person) CN() string {
-	return fmt.Sprintf("%s:mailto:%s", p.name, p.email)
 }
 
 func getCalendarEvents(db *sql.DB, year int) (events []event, err error) {
@@ -160,100 +127,4 @@ func getCalendarEvents(db *sql.DB, year int) (events []event, err error) {
 	}
 
 	return
-}
-
-type context struct {
-	db        *sql.DB
-	templates *template.Template
-}
-
-func (c *context) index(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
-	data := struct {
-		Year int
-	}{
-		time.Now().Year(),
-	}
-
-	c.render(w, "index", data)
-}
-
-func (c *context) calendar(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/calendar")
-
-	year, _ := strconv.Atoi(time.Now().Format("2006"))
-	events, err := getCalendarEvents(c.db, year)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
-	c.render(w, "rennen.ics", events)
-}
-
-func (c *context) render(w http.ResponseWriter, tmpl string, data interface{}) {
-	if err := c.templates.ExecuteTemplate(w, tmpl+".tmpl", data); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func enableCORS(fn http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		scheme := "http"
-		if r.TLS != nil {
-			scheme = "https"
-		}
-		w.Header().Set("Access-Control-Allow-Origin", scheme+"://"+r.Host)
-
-		fn(w, r)
-	}
-}
-
-func track(fn http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if os.Getenv("ENV") != "production" {
-			defer func(start time.Time, r *http.Request) {
-				elapsed := time.Since(start)
-				log.Printf("%s %s %s", r.Method, r.URL, elapsed)
-			}(time.Now(), r)
-		}
-
-		fn(w, r)
-	}
-}
-
-func init() {
-	//runtime.GOMAXPROCS(runtime.NumCPU())
-}
-
-func main() {
-	db, err := sql.Open("postgres", os.Getenv("DATABASE"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	c := context{
-		templates: template.Must(template.ParseGlob("./views/*.tmpl")),
-		db:        db,
-	}
-
-	m := http.NewServeMux()
-	m.Handle("/css/", http.FileServer(http.Dir("./static/")))
-	m.Handle("/img/", http.FileServer(http.Dir("./static/")))
-
-	m.Handle("/", track(enableCORS(c.index)))
-	m.Handle("/rennen.ics", track(enableCORS(c.calendar)))
-
-	s := &http.Server{
-		Addr:         ":" + os.Getenv("PORT"),
-		Handler:      m,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		// Go1.8
-		// IdleTimeout: 120 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-
-	log.Fatal(s.ListenAndServe())
 }
