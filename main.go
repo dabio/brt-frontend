ceb6cb15ee242b4b131369cf70d4527dbd2a2318
Danilo <dabio@users.noreply.github.com>
2017-02-07 06:57:54 +0100

    Remove runtime again

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"runtime"
 	"strconv"
 	"time"
 
@@ -74,7 +73,7 @@ func track(fn http.HandlerFunc) http.HandlerFunc {
 }
 
 func init() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
+	//	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
 func main() {
